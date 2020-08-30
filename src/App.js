4bd366faf87e4b8223791c080e61f1ff83e608d2
Dmitry Uvarov <diman_niks@mail.ru>
2020-08-30 15:52:07 +0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { menu } from './assets/constants';
+import Header from './components/header';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      score: 0,
+      menu: menu,
     };
   }
 
   render() {
-    
+    return (
+      <div className="App">
+        <Header list={menu} score={this.state.score} />
+      </div>
+    );
   }
 }
 
