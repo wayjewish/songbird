c010e636778f492c186e34bd44d792d59499a911
Dmitry Uvarov <diman_niks@mail.ru>
2020-08-30 16:02:46 +0300

    feat: gameEnd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import data from './assets/data';
-import { getRandomInRange } from './assets/utils';
+import { getRandomInRange, soundClick } from './assets/utils';
 import { menu } from './assets/constants';
 import Header from './components/header';
 import Game from './components/game';
 import GameList from './components/gameList';
 import GameDetail from './components/gameDetail';
+import EndGame from './components/endGame';
 
 
 class App extends React.Component {
@@ -26,7 +27,104 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    console.log(this.state.correctGames.name);
+  }
+
+  handleClickGameList(index) {
+    let arrGamesNew = this.state.arrGames;
+    let guessGames = arrGamesNew[index];
+
+    if (guessGames.id === this.state.correctGames.id && !this.state.levelCompleted) {
+      arrGamesNew[index].success = true;
+      soundClick(`${process.env.PUBLIC_URL}/sound/win.mp3`);
+
+      this.setState((state) => {
+        return {
+          score: state.score + state.scoreLevel,
+          levelCompleted: true,
+        };
+      });
+    } 
+
+    if (guessGames.id !== this.state.correctGames.id && !this.state.levelCompleted) { 
+      arrGamesNew[index].error = true;
+      soundClick(`${process.env.PUBLIC_URL}/sound/error.mp3`);
+
+      this.setState((state) => {
+        return {
+          scoreLevel: state.scoreLevel - 1,
+        };
+      });
+    }
+
+    this.setState((state) => {
+      return {
+        arrGames: arrGamesNew,
+        guessGames: guessGames,
+      };
+    });
+  }
+
+  handleClickNext() {
+    let levelNext = this.state.level + 1;
+
+    if (levelNext < this.state.levels) {
+      this.setState((state) => {
+        state.menu[state.level].isActive = false;
+        state.menu[levelNext].isActive = true;
+        let correctGames = data[levelNext][getRandomInRange(0, data[levelNext].length - 1)];
+        console.log(correctGames.name);
+
+        return {
+          menu: menu,
+          scoreLevel: 5,
+          level: state.level + 1,
+          levelCompleted: false,
+          arrGames: JSON.parse(JSON.stringify(data[levelNext])),
+          correctGames: correctGames,
+          guessGames: null,
+        };
+      });
+    }
+    
+    if (levelNext >= this.state.levels) {
+      this.setState((state) => {
+        return {
+          endGame: true,
+        };
+      });
+    }
+  }
+
+  handleClickNewGame() {
+    this.setState((state) => {
+      state.menu[this.state.level].isActive = false;
+      state.menu[0].isActive = true;
+
+      return {
+        menu: menu,
+        score: 0,
+        scoreLevel: 5,
+        level: 0,
+        levelCompleted: false,
+        arrGames: JSON.parse(JSON.stringify(data[0])),
+        correctGames: data[0][getRandomInRange(0, data[0].length - 1)],
+        guessGames: null,
+        endGame: false,
+      };
+    });
+  }
+
   render() {
+    if (this.state.endGame) {
+      return (
+        <div className="App">
+          <Header list={menu} score={this.state.score} />
+          <EndGame score={this.state.score} scoreMax={this.state.scoreMax} onEndGame={() => this.handleClickNewGame()} />
+        </div>
+      );
+    } else {
       let classesBtn = `btn${this.state.levelCompleted ? ' next' : ''}`;
 
       return (
@@ -44,6 +142,8 @@ class App extends React.Component {
           <button className={classesBtn} onClick={() => this.handleClickNext()}>Далее</button>
         </div>
       );
+    }
+
   }
 }
 
--- src/assets/utils.js
+++ src/assets/utils.js
@@ -2,6 +2,12 @@ function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function soundClick(src) {
+  var audio = new Audio();
+  audio.src = src;
+  audio.autoplay = true;
+}
+
 export {
-  getRandomInRange,
+  getRandomInRange, soundClick
 };
